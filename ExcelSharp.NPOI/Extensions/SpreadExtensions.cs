72cf6629e3ef0a3c99479846edd6ff9432cd6420
zmjack <jack@zmland.com>
2024-07-25 03:15:47 +0800

    ExcelSharp.NPOI.0.22.0

--- ExcelSharp.NPOI/Extensions/SpreadExtensions.cs
+++ ExcelSharp.NPOI/Extensions/SpreadExtensions.cs
@@ -6,26 +6,34 @@ namespace ExcelSharp.NPOI;
 [EditorBrowsable(EditorBrowsableState.Never)]
 public static class SpreadExtensions
 {
-    public static ExcelBook ToExcelBook(this ISpreadBook @this)
+    public static ExcelBook ToExcelBook(this ISpreadBook @this, bool autoSize = true)
     {
         var book = new ExcelBook(ExcelVersion.Excel2007);
         foreach (var spsheet in @this.Sheets)
         {
             var sheet = book.CreateSheet(spsheet.Name);
             sheet.PrintSpreadSheet(spsheet);
-            sheet.AutoSize([.. RangeEx.Create(0, spsheet.ColumnLength)]);
+
+            if (autoSize)
+            {
+                sheet.AutoSize([.. RangeEx.Create(0, spsheet.ColumnLength)]);
+            }
         }
         return book;
     }
 
-    public static ExcelBook ToExcelBook(this ISpreadBook @this, Cursor cursor)
+    public static ExcelBook ToExcelBook(this ISpreadBook @this, Cursor cursor, bool autoSize = true)
     {
         var book = new ExcelBook(ExcelVersion.Excel2007);
         foreach (var spsheet in @this.Sheets)
         {
             var sheet = book.CreateSheet(spsheet.Name);
             sheet.PrintSpreadSheet(spsheet, cursor);
-            sheet.AutoSize([.. RangeEx.Create(0, spsheet.ColumnLength)]);
+
+            if (autoSize)
+            {
+                sheet.AutoSize([.. RangeEx.Create(0, spsheet.ColumnLength)]);
+            }
         }
         return book;
     }
