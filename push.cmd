5e3bbf47cf08170a191eea5cf160de940adc2ac7
zmjack <jack@zmland.com>
2025-04-03 15:00:44 +0800

    ExcelSharp.NPOI.0.30.1

--- push.cmd
+++ push.cmd
@@ -1,2 +1,2 @@
-nuget push "ExcelSharp.NPOI/bin/Release/ExcelSharp.NPOI.0.30.0.nupkg" -source nuget.org
+nuget push "ExcelSharp.NPOI/bin/Release/ExcelSharp.NPOI.0.30.1.nupkg" -source nuget.org
 pause
